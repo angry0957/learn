96bd5bafd5378193afddf10b1e0e053ff7d1b7a9
Abdul Rahman <abdul.095722@gmail.com>
2018-10-30 10:59:23 +0500

    b1 Component created

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <App href="https://www.youtube.com" text="Youtube"/>,
+    <App href="https://www.google.com" text="Google"/>, 
+    <App href="https://www.facebook.com" text="Facebook"/>,
+  </div>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
